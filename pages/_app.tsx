6ed81d4c8737e6d0cd1ef6d432bbe4ed0d291283
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-05-22 22:32:44 -0400

    fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
-const activeChain = "mumbai";
+const activeChain = "goerli";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
